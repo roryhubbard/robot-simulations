49ffaea5476fafdb8ed054363f84a93e9887153a
Rory Hubbard <calvin3h@gmail.com>
2021-12-14 01:21:43 +0100

    setting up gait optimization for simple dog

--- main.py
+++ main.py
@@ -1,48 +1,38 @@
 import numpy as np
-
 from pydrake.multibody.parsing import Parser
 from pydrake.multibody.plant import AddMultibodyPlantSceneGraph
-from pydrake.systems.analysis import Simulator
 from pydrake.systems.framework import DiagramBuilder
 from pydrake.systems.meshcat_visualizer import ConnectMeshcatVisualizer
 
 
+def set_home(plant, context):
+  plant.GetJointByName("hip_front").set_angle(context, -np.pi/4)
+  plant.GetJointByName("hip_back").set_angle(context, np.pi/4)
+  plant.GetJointByName("knee_front").set_angle(context, np.pi/2)
+  plant.GetJointByName("knee_back").set_angle(context, -np.pi/2)
+
+
 def main():
   builder = DiagramBuilder()
-  plant, scene_graph = AddMultibodyPlantSceneGraph(builder, 0.0)
-
+  plant, scene_graph = AddMultibodyPlantSceneGraph(builder, 1e-3)
   parser = Parser(plant)
-
-  simple_dog_file = "models/simple_dog_2d.sdf"
-  simple_dog = parser.AddModelFromFile(simple_dog_file, model_name="simple_dog")
-
-  ground_file = "models/little_dog/ground.urdf"
-  ground = parser.AddModelFromFile(ground_file, model_name="ground")
-
-  meshcat_vis = ConnectMeshcatVisualizer(builder, scene_graph,
-                                         zmq_url="tcp://127.0.0.1:6000")
-
+  simple_dog = parser.AddModelFromFile("models/simple_dog_2d.sdf")
+  ground = parser.AddModelFromFile("models/little_dog/ground.urdf")
   plant.Finalize()
+  visualizer = ConnectMeshcatVisualizer(builder, scene_graph,
+                                        zmq_url="tcp://127.0.0.1:6000")
   diagram = builder.Build()
+  context = diagram.CreateDefaultContext()
+  plant_context = plant.GetMyContextFromRoot(context)
+  set_home(plant, plant_context)
+  visualizer.load()
+  diagram.Publish(context)
 
-  # Create a new simulator (with its own implicitly created context).
-  simulator = Simulator(diagram)
-  # Set initial conditions.
-  sim_plant_context = plant.GetMyContextFromRoot(simulator.get_mutable_context())
+  q0 = plant.GetPositions(plant_context)
 
-  plant.GetJointByName("knee_front").set_angle(sim_plant_context, .5)
-  plant.GetJointByName("knee_back").set_angle(sim_plant_context, -.5)
-  #plant.get_actuation_input_port(simple_dog).FixValue(sim_plant_context, np.zeros((4,1)))
-
-  # Reset the recording (in case you are running this cell more than once).
-  meshcat_vis.reset_recording()
-
-  # Start recording and simulate.
-  meshcat_vis.start_recording()
-  simulator.AdvanceTo(1.0)
-
-  # Publish the recording to meshcat.
-  meshcat_vis.publish_recording()
+  #visualizer.reset_recording()
+  #visualizer.start_recording()
+  #visualizer.publish_recording()
 
 
 if __name__ == "__main__":
