808f5cadfeaeb983c88bdf9484da8b50170d3e3d
Rory Hubbard <calvin3h@gmail.com>
2021-12-17 01:24:17 +0100

    simple obstacle avoidance working

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import numpy as np
+import cvxpy as cp
 import matplotlib.pyplot as plt
 from pydrake.multibody.parsing import Parser
 from pydrake.multibody.plant import AddMultibodyPlantSceneGraph
 from pydrake.systems.framework import DiagramBuilder
 from pydrake.systems.meshcat_visualizer import ConnectMeshcatVisualizer
-from pydrake.all import Solve, LinearSystem, DirectTranscription, RigidTransform
+from pydrake.all import RigidTransform
 
 
-def main():
-  # Discrete-time approximation of the double integrator.
-  dt = 0.01;
+def make_system():
+  dt = 0.1;
   A_tile = np.array([
     [1., dt],
     [0., 1.],
@@ -25,65 +25,95 @@ def main():
   ])
   C = np.eye(6)
   D = np.zeros((6, 2))
-  sys = LinearSystem(A, B, C, D, dt)
+  return A, B, C, D, dt
 
+
+def drake_setup():
   builder = DiagramBuilder()
   plant, scene_graph = AddMultibodyPlantSceneGraph(builder, 1e-3)
   parser = Parser(plant)
-  sphere = parser.AddModelFromFile('models/sphere.sdf')
+  parser.AddModelFromFile('models/sphere.sdf')
   plant.Finalize()
   visualizer = ConnectMeshcatVisualizer(builder, scene_graph,
                                         zmq_url='tcp://127.0.0.1:6000')
   diagram = builder.Build()
   context = diagram.CreateDefaultContext()
   plant_context = plant.GetMyContextFromRoot(context)
+  return plant, visualizer, diagram, context, plant_context
+
+
+def main():
+  plant, visualizer, diagram, context, plant_context = drake_setup()
+  A, B, C, D, dt = make_system()
 
   x0 = [-2., 0., -2., 0., 0., 0.]
-  xf = [0., 0., 0., 0., 0., 0.]
+  xN = [2., 0., 2., 0., 0., 0.]
 
   plant.SetFreeBodyPose(plant_context, plant.GetBodyByName('body'),
                         RigidTransform(x0[::2]))
   visualizer.load()
   diagram.Publish(context)
 
-  N = 284
-  prog = DirectTranscription(sys, sys.CreateDefaultContext(), N)
-  prog.AddBoundingBoxConstraint(x0, x0, prog.initial_state())
-  prog.AddBoundingBoxConstraint(xf, xf, prog.final_state())
-  x = prog.state()
-  u = prog.input()
-  #prog.AddRunningCost(10*x[0]**2 + x[1]**2)
-  #prog.AddRunningCost(u**2)
-  prog.AddConstraintToAllKnotPoints(u[0] <= 1)
-  prog.AddConstraintToAllKnotPoints(u[0] >= -1)
-  prog.AddConstraintToAllKnotPoints(u[1] <= 1)
-  prog.AddConstraintToAllKnotPoints(u[1] >= -1)
+  # http://www.cs.cmu.edu/~zkolter/course/15-780-s14/mip.pdf
+  u_limit = np.ones(2) * 10.
+  null_control = np.zeros(2)
+  obs_lbx = -1
+  obs_ubx = 1
+  obs_lby = -1
+  obs_uby = 1
+  bigM = 10
+  N = 100
 
-  result = Solve(prog)
-  assert(result.is_success()), "Optimization failed"
+  x = cp.Variable((N, A.shape[0]))
+  u = cp.Variable((N, B.shape[1]))
+  z1 = cp.Variable(N, integer=True)
+  z2 = cp.Variable(N, integer=True)
+  z3 = cp.Variable(N, integer=True)
+  z4 = cp.Variable(N, integer=True)
 
-  traj = prog.ReconstructStateTrajectory(result)
-  t = traj.get_segment_times()
-  x_values = traj.vector_values(t)
-  x_sol = x_values[0, :]
-  y_sol = x_values[2, :]
-  z_sol = x_values[4, :]
+  constraints = []
+  cost = 0
+  for n in range(N-1):
+    # obstacle avoidance using big M technique
+    constraints += [x[n, 0] <= obs_lbx + z1[n] * bigM]
+    constraints += [x[n, 0] >= obs_ubx - z2[n] * bigM]
+    constraints += [x[n, 2] <= obs_lbx + z3[n] * bigM]
+    constraints += [x[n, 2] >= obs_ubx - z4[n] * bigM]
+    constraints += [z1 + z2 + z3 + z4 <= 3]
 
-  #plt.figure()
-  #plt.plot(x_values[0,:], x_values[1,:])
-  #plt.xlabel('x_sol')
-  #plt.ylabel('qdot');
-  #plt.show()
-  #plt.close()
+    # dynamics
+    constraints += [x[n+1] == A @ x[n] + B @ u[n]]
+
+    # control limits
+    constraints += [u[n] <= u_limit]
+
+    # smooth trajectory
+    cost += cp.sum_squares(x[n+1] - x[n])
+
+  # start and final states
+  constraints += [x[0] == x0, x[N-1] == xN]
+  # final control
+  constraints += [u[N-1] == null_control]
+  problem = cp.Problem(cp.Minimize(cost), constraints)
+  problem.solve(solver=cp.GUROBI, verbose=True)
 
   visualizer.start_recording()
-  for i in range(len(x_sol)):
-    context.SetTime(t[i])
+  for i in range(N):
+    context.SetTime(dt*i)
     plant.SetFreeBodyPose(plant_context, plant.GetBodyByName('body'),
-                          RigidTransform([x_sol[i], y_sol[i], z_sol[i]]))
+                          RigidTransform(x[i, ::2].value))
+
     diagram.Publish(context)
   visualizer.stop_recording()
   visualizer.publish_recording()
+
+#  fig, ax = plt.subplots()
+#  ax.plot(x[:, 0].value, x[:, 2].value)
+#  obs_x = [obs_lbx, obs_lbx, obs_ubx, obs_ubx, obs_lbx]
+#  obs_y = [obs_lby, obs_uby, obs_uby, obs_lby, obs_lby]
+#  ax.plot(obs_x, obs_y)
+#  plt.show()
+#  plt.close()
 
 
 if __name__ == '__main__':
