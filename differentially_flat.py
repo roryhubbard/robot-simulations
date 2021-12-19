008049ebe67941cf5eceed6e39e927a679565900
Rory Hubbard <calvin3h@gmail.com>
2021-12-19 13:38:54 +0100

    rotated obstacle avoidance

--- differentially_flat.py
+++ differentially_flat.py
@@ -31,8 +31,8 @@ class DifferentiallyFlatTrajectory:
       self.cost += cp.sum_squares(self.spline_coefficients[s][:, -1])
       spline = self.eval(s, 0)
       next_spline = self.eval(s+1, 0)
-      for i in range(self.n_flat_outputs):
-        self.cost += cp.sum_squares(next_spline[i] - spline[i])
+      for z in range(self.n_flat_outputs):
+        self.cost += cp.sum_squares(next_spline[z] - spline[z])
 
   def add_continuity_constraints(self):
     for s in range(self.sample_times.size-1):
@@ -40,7 +40,7 @@ class DifferentiallyFlatTrajectory:
 
       for z in range(self.n_flat_outputs):
         for sd in range(self.smoothness_degree+1):
-          spline_end = self.eval_spline_derivative(
+          spline_end = self.eval_spline(
             h, sd, self.spline_coefficients[s][z])
           next_spline_start = self.spline_coefficients[s+1][z, sd] \
             * np.math.factorial(sd)
@@ -49,7 +49,8 @@ class DifferentiallyFlatTrajectory:
 
   def add_constraint(self, t, derivative_order, bounds,
                      equality=False, greater_than=False):
-    """ Add constraint to all flat outputs of specified derivative order
+    """
+    Add constraint to all flat outputs of specified derivative order
     """
     flat_outputs = self.eval(t, derivative_order)
     for z in range(len(flat_outputs)):
@@ -69,16 +70,15 @@ class DifferentiallyFlatTrajectory:
       else:
         self.constraints += [lhs <= rhs]
 
-  def add_obstacle(self, vertices, bigM=10):
+  def add_obstacle(self, vertices, checkpoints, bigM=10):
     """
     vertices: list(tuple(float)) = coordinates specifying vertices of obstacle
       - counterclockwise ordering and closed (first element == last elements)
     """
     eps = 1e-6
-    b = cp.Variable((self.sample_times.size, len(vertices)-1), boolean=True)
+    b = cp.Variable((checkpoints.size, len(vertices)-1), boolean=True)
 
-    for s in range(self.sample_times.size):
-      t = self.sample_times[s]
+    for t in checkpoints:
       flat_outputs = self.eval(t, 0)
 
       for i in range(len(vertices)-1):
@@ -86,40 +86,39 @@ class DifferentiallyFlatTrajectory:
         v2 = vertices[i+1]
         y_delta = v2[1] - v1[1]
         x_delta = v2[0] - v1[0]
-        greater_than = False
+        greater_than = True
         if abs(y_delta) < eps:
           # horizontal
           rhs = v2[1]
           lhs_idx = 1
-          if x_delta < 0:
-            greater_than = True
+          if x_delta > 0:
+            greater_than = False
         elif abs(x_delta) < eps:
           # vertical
           rhs = v2[0]
           lhs_idx = 0
-          if y_delta > 0:
-            greater_than = True
+          if y_delta < 0:
+            greater_than = False
         else:
           # slanted
-          # TODO: this doesn't work
           m = y_delta / x_delta
           y_intercept = v2[1] - m * v2[0]
-          rhs = y_intercept / m
-          lhs_idx = 0
+          rhs = m * flat_outputs[0] + y_intercept
+          lhs_idx = 1
           theta = np.arctan2(y_delta, x_delta)
-          if theta > 0:
-            greater_than = True
+          if -np.pi / 2 < theta < np.pi / 2:
+            greater_than = False
 
         if greater_than:
-          bigM_rhs = rhs - b[s, i] * bigM
+          bigM_rhs = rhs - b[t, i] * bigM
         else:
-          bigM_rhs = rhs + b[s, i] * bigM
+          bigM_rhs = rhs + b[t, i] * bigM
 
         lhs = flat_outputs[lhs_idx]
         self.add_single_constraint(lhs, bigM_rhs, greater_than=greater_than)
-        self.add_single_constraint(cp.sum(b[s]), len(vertices)-2)
+        self.add_single_constraint(cp.sum(b[t]), len(vertices)-2)
 
-  def eval_spline_derivative(self, h, d, c):
+  def eval_spline(self, h, d, c):
     """
     h: float = time relative to start of spline
     d: int = derivative order
@@ -146,7 +145,7 @@ class DifferentiallyFlatTrajectory:
     d = derivative_order
     flat_outputs = []
     for z in range(self.n_flat_outputs):
-      flat_outputs.append(self.eval_spline_derivative(h, d, c[z]))
+      flat_outputs.append(self.eval_spline(h, d, c[z]))
     
     return flat_outputs
 
@@ -161,6 +160,7 @@ def rotate(theta, vertices):
     [np.sin(theta), np.cos(theta)],
   ])
   return (R @ vertices.T).T
+
 
 def main():
   N = 22
@@ -189,9 +189,10 @@ def main():
     [1, 1],
   ])
   theta = np.pi / 4
-  #square = rotate(theta, square)
+  square = rotate(theta, square)
 
-  dft.add_obstacle(square)
+  checkpoints = np.linspace(t0, tf, 44)
+  dft.add_obstacle(square, checkpoints)
 
   dft.solve()
 
@@ -204,7 +205,7 @@ def main():
     y.append(flat_outputs[1])
 
   fig, ax = plt.subplots()
-  ax.plot(x, y, '*')
+  ax.plot(x, y, '.')
   ax.plot(*zip(*square))
   plt.show()
   plt.close()
